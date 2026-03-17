d2c2fa45863eabfdac9d64f34ff7ba19d0bab0ec
alina-kozlyuk <Talina787@icloud.com>
2026-03-17 11:23:26 +0000

    Deploying to gh-pages from @ alina-kozlyuk/weather-app@af563887774d4b2f976760915ffbdeec4ad15b58 🚀

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-import{a as u,S as p,i}from"./assets/vendor-DQvd0HNi.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();function d(n){const s="https://pixabay.com",t="/api/",o=new URLSearchParams({key:"55046527-8fc3f9f8db0162eee6eb185b5",q:n,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=s+t+"?"+o;return u.get(e).then(r=>r.data)}const l=document.querySelector(".gallery"),f=document.querySelector(".loader"),m=new p(".gallery a",{captionsData:"alt",captionDelay:250});function y(n){const s=n.map(t=>`
-        <li class="gallery-item">
-      <a href="${t.largeImageURL}">
-        <img class="gallery-image" src="${t.webformatURL}" alt="${t.tags}" loading="lazy" />
-      </a>
-      <div class="info">
-        <p>Likes: ${t.likes}</p>
-        <p>Views: ${t.views}</p>
-        <p>Comments: ${t.comments}</p>
-        <p>Downloads: ${t.downloads}</p>
-      </div>
-    </li>
-        `).join("");l.insertAdjacentHTML("beforeend",s),m.refresh()}function g(){l.innerHTML=""}function h(){f.style.display="block"}function c(){f.style.display="none"}const L={form:document.querySelector(".form")};L.form.addEventListener("submit",n=>{n.preventDefault();const t=new FormData(n.target).get("search-text").trim();if(t===""){i.error({message:"Please enter a search query!",position:"topRight"});return}g(),h(),d(t).then(o=>{c(),o.hits.length===0?i.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):y(o.hits)}).catch(o=>{c(),i.error({message:"Oops, something went wrong!",position:"topRight"}),console.log(o)})});
+import{a as c,i as a}from"./assets/vendor-0JmyGMd6.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();function l(n){return c.get("https://geocoding-api.open-meteo.com/v1/search",{params:{name:n,count:1}}).then(r=>{var t;const o=(t=r.data.results)==null?void 0:t[0];if(!o)throw new Error("City not found");const{latitude:i,longitude:e}=o;return c.get("https://api.open-meteo.com/v1/forecast",{params:{latitude:i,longitude:e,current_weather:!0}})}).then(r=>r.data)}const u=document.querySelector("#weather-container"),d=document.querySelector(".loader");function f(n){const{temperature:r,windspeed:o}=n.current_weather,i=`<div class="weather-card">
+      <h2>Поточна погода</h2>
+      <p>🌡️ Температура: ${r}°C</p>
+      <p>💨 Вітер: ${o} км/год</p>
+    </div>
+  `;u.innerHTML=i}function p(){u.innerHTML=""}function m(){d.classList.remove("hidden")}function h(){d.classList.add("hidden")}const g={form:document.querySelector(".form")};g.form.addEventListener("submit",n=>{n.preventDefault();const r=n.target.elements.city.value.trim();if(!r){a.error({message:"Введіть, будь ласка, місто",position:"topRight"});return}p(),m(),l(r).then(o=>{h(),f(o)}).catch(()=>{a.error({message:"Місто не знайдено",position:"topRight"})})});
 //# sourceMappingURL=index.js.map
